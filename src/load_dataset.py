5718fd1f00c576021b5c09df21e67cc1da62ad97
hungchiayu1 <72308196+hungchiayu1@users.noreply.github.com>
2022-12-10 21:26:39 +0800

    Revert "Added files"
    This reverts commit e6c0a821960ff04b090a410613b9ec11d5881174.

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -11,16 +11,15 @@ def download_dataset(batch_size=128):
          transforms.Normalize((0.49139968, 0.48215827 ,0.44653124), (0.24703233 ,0.24348505 ,0.26158768))])
 
     batch_size = 128
-    print("Downloading dataset -----")
-    trainset = torchvision.datasets.CIFAR10(root='../../data', train=True,
+
+    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                             download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                               shuffle=True, num_workers=2)
 
-    testset = torchvision.datasets.CIFAR10(root='../../data', train=False,
+    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                            download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                              shuffle=False, num_workers=2)
-    print("Finish Downloading dataset -----")
-    
+
     return trainloader,testloader
